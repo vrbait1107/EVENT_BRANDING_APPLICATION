7a3132c1225d6b764ca218b5bc5bc3c2f89cc6e9
Vishal_Bait <vishalbait01@gmail.com>
2020-04-08 22:29:41 +0530

    Now Main Admin can  only Add Faculty Coordinators changes in adminIndex.php

--- Admin/adminIndex.php
+++ Admin/adminIndex.php
@@ -2,10 +2,12 @@
 require_once "../config.php";
 session_start();
 
-
 // Checking if Admin is Login or Not if Not Login Sending to the Admin Login Page
-if(!isset($_SESSION['adminEmail']) &&  $_SESSION['adminType'] && $_SESSION['adminDepartment'] && $_SESSION['adminEvent']){
-    header('Location:adminLogin.php');
+if(!isset($_SESSION['adminEmail']) && 
+$_SESSION['adminType'] &&
+$_SESSION['adminDepartment'] &&
+$_SESSION['adminEvent']){
+header('Location:adminLogin.php');
 }
 ?>
 
@@ -19,25 +21,80 @@ if(!isset($_SESSION['adminEmail']) &&  $_SESSION['adminType'] && $_SESSION['admi
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Administrator</title>
+
+    <!--Local StyleSheet-->
     <link href="css/styles.css" rel="stylesheet" />
+    <!--DataTable Bootstrap-->
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
         crossorigin="anonymous" />
+    <!--Font-Awesome-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js"
         crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-        <style>
-        .btn-block{
-            border-radius:30px;
-        }
-        </style>
+    <!--SweetAlert.js-->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
 </head>
 
 
 
 <body class="sb-nav-fixed">
+
+    <?php 
+    
+    // Inserting Data into admin_information table Database Name is user_registration
+    
+    if(isset($_POST['addAdmin'])){
+    
+       $email= $_POST['email'];
+       $adminType = $_POST['adminType'];
+       $adminDepartment = $_POST['adminDepartment'];
+       $adminEvent = $_POST['adminEvent'];
+       $password = $_POST['password'];
+       $password= password_hash($password,PASSWORD_BCRYPT);
+    
+       $sql = "select * from admin_information where admin_information.email = '$email'";
+       $result = mysqli_query($conn,$sql);
+    
+       if(mysqli_num_rows($result)>0) {
+    
+        echo "<script>Swal.fire({
+                icon: 'warning',
+                title: 'Unable to Insert Data',
+                text: 'Admin Profile Already Exist'
+            })</script>";
+      }
+       else{
+    
+        $sql = "insert into admin_information (email, adminType, adminDepartment, adminEvent, 
+        adminPassword) VALUES ('$email','$adminType', '$adminDepartment','$adminEvent', '$password')";
+        $result= mysqli_query($conn,$sql);
+    
+        if($result) {
+            echo "<script>Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: 'Successfully Inserted Admin Profile'
+                })</script>";
+        }
+        else {
+            echo "<script>Swal.fire({
+                    icon: 'error',
+                    title: 'ERROR',
+                    text: 'Something Went Wrong'
+                })</script>";
+        }
+    
+    }
+    }
+    
+    // Inserting data into admin_information is Completed
+    ?>
+
+
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a class="navbar-brand" href="adminIndex.php">Administrator</a><button class="btn btn-link btn-sm order-1 order-lg-0"
-            id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button><!-- Navbar Search-->
+        <a class="navbar-brand" href="adminIndex.php">Administrator</a><button
+            class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i
+                class="fas fa-bars"></i></button><!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
             <div class="input-group">
                 <input class="form-control" type="text" placeholder="Search for..." aria-label="Search"
@@ -70,30 +127,43 @@ if(!isset($_SESSION['adminEmail']) &&  $_SESSION['adminType'] && $_SESSION['admi
                 <div class="sb-sidenav-menu">
                     <div class="nav">
                         <div class="sb-sidenav-menu-heading">Core</div>
+
                         <a class="nav-link" href="adminIndex.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
+
                         <div class="sb-sidenav-menu-heading">Interface</div>
+
                         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                             aria-expanded="false" aria-controls="collapsePages">
                             <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                             Pages
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
+
                         <div class="collapse" id="collapsePages" aria-labelledby="headingTwo"
                             data-parent="#sidenavAccordion">
+
                             <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+
                                 <a class="nav-link collapsed" href="#" data-toggle="collapse"
                                     data-target="#pagesCollapseAuth" aria-expanded="false"
-                                    aria-controls="pagesCollapseAuth">Authentication
-                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div></a>
+                                    aria-controls="pagesCollapseAuth">
+                                    Authentication
+                                    <div class="sb-sidenav-collapse-arrow">
+                                        <i class="fas fa-angle-down"></i>
+                                        <div>
+                                </a>
+
                                 <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
                                     data-parent="#sidenavAccordionPages">
-                                    <nav class="sb-sidenav-menu-nested nav"><a class="nav-link"
-                                            href="adminLogin.php">Login</a><a class="nav-link"
-                                            href="password.php">Change Password</a></nav>
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="adminLogin.php">Login</a>
+                                        <a class="nav-link" href="password.php">Change Password</a>
+                                    </nav>
                                 </div>
+
                             </nav>
                         </div>
 
@@ -101,12 +171,19 @@ if(!isset($_SESSION['adminEmail']) &&  $_SESSION['adminType'] && $_SESSION['admi
 
                         <div class="sb-sidenav-menu-heading">Addons</div>
                         <a class="nav-link" href="charts.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            <div class="sb-nav-link-icon">
+                                <i class="fas fa-chart-area"></i>
+                            </div>
                             Charts
-                        </a><a class="nav-link" href="adminIndexData.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        </a>
+
+                        <a class="nav-link" href="adminIndexData.php">
+                            <div class="sb-nav-link-icon">
+                                <i class="fas fa-table"></i>
+                            </div>
                             Tables
                         </a>
+
                     </div>
                 </div>
 
@@ -129,142 +206,67 @@ if(!isset($_SESSION['adminEmail']) &&  $_SESSION['adminType'] && $_SESSION['admi
 
                     <!--  ADD Administartor Profile Form  -->
                     <div class="row">
-                        <div class="col"></div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 offset-md-3">
 
+                            <h5 class="text-danger text-center mb-3">Note: Main Addministrator Can Add Only Faculty
+                                Coordinators</h5>
 
-
-                        <?php 
-
-// Inserting Data into admin_information table Database Name is user_registration
-
-if(isset($_POST['login'])){
-
-   $email= $_POST['email'];
-   $adminType = $_POST['adminType'];
-   $adminDepartment = $_POST['adminDepartment'];
-   $adminEvent = $_POST['adminEvent'];
-   $password = $_POST['password'];
-
-   $password= password_hash($password,PASSWORD_BCRYPT);
-
-   $sql = "select* from admin_information where admin_information.email = '$email'";
-   $result = mysqli_query($conn,$sql);
-
-   if(mysqli_num_rows($result)>0) {
-
-    echo "<script>Swal.fire({
-        icon: 'warning',
-        title: 'Unable to Insert Data',
-        text: 'Admin Profile Already Exist'
-      })</script>";
-  }
-   else{
-
-    $sql = "insert into admin_information (email, adminType, adminDepartment, adminEvent, 
-    adminPassword) VALUES ('$email','$adminType', '$adminDepartment','$adminEvent', '$password')";
-    $result= mysqli_query($conn,$sql);
-
-    if($result) {
-        echo "<script>Swal.fire({
-            icon: 'success',
-            title: 'Success',
-            text: 'Successfully Inserted Admin Profile'
-          })</script>";
-    }
-    else {
-        echo "<script>Swal.fire({
-            icon: 'error',
-            title: 'ERROR',
-            text: 'Something Went Wrong'
-          })</script>";
-    }
-
-}
-}
-
-// Inserting data into admin_infprmation is Completed
-?>
-
-
-
-                     
                             <h3 class="text-white text-center bg-info mb-4 p-2"> ADD ADMINISTRATOR PROFILE </h3>
+
                             <form action="" method="post">
-                            <div class="form-group">
-                            <label>Enter Your Email</label>
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Email"
-                                required>
-                        </div>
-
-                       
-                        <div class="form-group">
-                            <label>Admin Type</label>
-                            <select class="form-control" name="adminType">
-                                <option value="Administrator">Administrator</option>
-                                <option value="Faculty Coordinator">Faculty Coordinator</option>
-                                <option value="Student Coordinator">Student Coordinator</option>
-                            </select>
-                        </div>
-
-
-                        <div class="form-group">
-                            <label>Admin Department</label>
-                            <select class="form-control" name="adminDepartment">
-                                <option value="Not Applicable">Not Applicable</option>
-                                <option value="Electronics & Telecommunication">Electronics & Telecommunication</option>
-                                <option value="Chemical">Chemical</option>
-                                <option value="Computer">Computer</option>
-                                <option value="Mechanical">Mechanical</option>
-                                <option value="Civil">Civil</option>
-                            </select>
-                        </div>
-
-
-                        <div class="form-group">
-                            <label>Admin Event</label>
-                            <select class="form-control" name="adminEvent">
-                                <option value="Not Applicable">Not Applicable</option>
-                                <option value="EXTC Paper Presentation">EXTC Paper Presentation</option>
-                                <option value="EXTC Poster Presentation">EXTC Poster Presentation</option>
-                                <option value="EXTC Project Presentation">EXTC Project Presentation</option>
-                                <option value="Tech Boss">Tech Boss</option>
-                                <option value="Fun Tech">Fun Tech</option>
-                                <option value="School Event">School Event</option>
-                                <option value="Logo Contest">Logo Contest</option>
-                                <option value="Calci War">Calci War</option>
-                                <option value="Chemical Paper Presentation">Chemical Paper Presentation</option>
-                                <option value="Chemical Poster Presentation">Chemical Poster Presentation</option>
-                                <option value="Chemical Project Presentation">Chemical Project Presentation</option>
-                                <option value="Computer Paper Presentation">Computer Paper Presentation</option>
-                                <option value="Computer Poster Presentation">Computer Poster Presentation</option>
-                                <option value="Computer Project Presentation">Computer Project Presentation</option>
-                                <option value="Mechanical Paper Presentation">Mechanical Paper Presentation</option>
-                                <option value="Mechanical Poster Presentation">Mechanical Poster Presentation</option>
-                                <option value="Mechanical Project Presentation">Mechanical Project Presentation</option>
-                                <option value="Civil Paper Presentation">Civil Paper Presentation</option>
-                                <option value="Civil Poster Presentation">Civil Poster Presentation</option>
-                                <option value="Civil Project Presentation">Civil Project Presentation</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control" name="password" id="password"
-                                placeholder="Password" required autocomplete="off">
-                        </div>
+                                <div class="form-group">
+                                    <label>Enter Your Email</label>
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="Email"
+                                        required>
+                                </div>
 
 
                                 <div class="form-group">
-                                    <input class="btn btn-primary btn-block mb-5" type="submit" class="form-control"
-                                        name="login" id="login" value="ADD PROFILE">
+                                    <label>Admin Type</label>
+                                    <select class="form-control" name="adminType">
+                                        <option value="Administrator">Administrator</option>
+                                        <option value="Faculty Coordinator">Faculty Coordinator</option>
+                                    </select>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label>Admin Department</label>
+                                    <select class="form-control" name="adminDepartment">
+                                        <option value="Not Applicable">Not Applicable</option>
+                                        <option value="Electronics & Telecommunication">Electronics & Telecommunication
+                                        </option>
+                                        <option value="Chemical">Chemical</option>
+                                        <option value="Computer">Computer</option>
+                                        <option value="Mechanical">Mechanical</option>
+                                        <option value="Civil">Civil</option>
+                                    </select>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label>Admin Event</label>
+                                    <select class="form-control" name="adminEvent">
+                                        <option value="Not Applicable">Not Applicable</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" name="password" id="password"
+                                        placeholder="Password" required autocomplete="off">
+                                </div>
+
+
+                                <div class="form-group">
+                                    <input class="btn btn-primary btn-block rounded-pill mb-5" type="submit"
+                                        class="form-control" name="addAdmin" id="addAdmin" value="ADD PROFILE">
                                 </div>
                             </form>
                         </div>
-                        <div class="col"></div>
                     </div>
 
-                     <!--  ADD Administartor Profile Form Complete -->
+                    <!--  ADD Administartor Profile Form Complete -->
 
                     <div class="row">
                         <div class="col-xl-6">
@@ -282,8 +284,6 @@ if(isset($_POST['login'])){
                     </div>
 
 
-
-
                     <div class="card mb-4">
                         <div class="card-header"><i class="fas fa-table mr-1"></i>DataTable Example</div>
                         <div class="card-body">
@@ -296,7 +296,6 @@ if(isset($_POST['login'])){
 
                     $sql = 'select * from user_information order by user_information.firstName ASC';
                     $result =mysqli_query($conn,$sql);
-
 
 
                      echo '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
@@ -334,6 +333,7 @@ if(isset($_POST['login'])){
                     </div>
                 </div>
             </main>
+
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid">
                     <div class="d-flex align-items-center justify-content-between small">
@@ -346,8 +346,11 @@ if(isset($_POST['login'])){
                     </div>
                 </div>
             </footer>
+
         </div>
     </div>
+
+    <!--Script-->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
