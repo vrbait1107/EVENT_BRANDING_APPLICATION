ed473618cfe5f8a53b3300134786c4acb08d3437
Vishal_Bait <vishalbait01@gmail.com>
2020-04-09 11:15:22 +0530

    Some Minor Formatting in login.php

--- login.php
+++ login.php
@@ -12,38 +12,40 @@ if(isset($_SESSION['user'])) {
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS, then eventReg.css,  then animate.css,  then font-awesome.css,  then sweetAlert.js -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/event-reg.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>GIT SHODH 2K20</title>
+  <!-- Bootstrap CSS, then eventReg.css,  then animate.css,  then font-awesome.css,  then sweetAlert.js -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/event-reg.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 
-    <style>
-       .fa-users{
-            border-radius:100%;
-            border: 2px solid #f0ad4e;
-            padding:10px;
-        }
+  <title>GIT SHODH 2K20</title>
 
-        .btn-block {
-          border-radius:30px;
-        }
+  <style>
+    .fa-users {
+      border-radius: 100%;
+      border: 2px solid #f0ad4e;
+      padding: 10px;
+    }
 
-        a {
-          text-decoration:none !important;
-        }
+    .btn-block {
+      border-radius: 30px;
+    }
 
-    </style>
-  </head>
-  <body>
+    a {
+      text-decoration: none !important;
+    }
+  </style>
+</head>
+
+<body>
 
 
 
@@ -117,50 +119,59 @@ require_once "config.php";
 
 
 
-    
-<main class="container my-4">
-  <div class="row">
-<section class="col-md-6 offset-md-3">
 
-<div class="card shadow p-5">
-<h2 class="text-center text-uppercase">GIT <span class="text-danger">SHODH</span> 2K20 LOGIN</h2>
-<hr>
+  <main class="container my-4">
+    <div class="row">
+      <section class="col-md-6 offset-md-3">
 
-<form action="" method="POST">
+        <div class="card shadow p-5">
+          <h2 class="text-center text-uppercase">GIT <span class="text-danger">SHODH</span> 2K20 LOGIN</h2>
+          <hr>
 
-<div class="text-center">
- <span class="text-warning"><i class="fa fa-users fa-5x"></i></span>
-</div>
+          <form action="" method="POST">
 
-  <div class="form-group">
-    <label for="Email">Username</label>
-    <input type="email" name="userName" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Enter Username">
-  </div>
+            <div class="text-center">
+              <span class="text-warning"><i class="fa fa-users fa-5x"></i></span>
+            </div>
 
-  <div class="form-group">
-    <label for="Password">Password</label>
-    <input type="password" name="password" class="form-control" id="Password" placeholder="Enter Password">
-  </div>
+            <div class="form-group">
+              <label for="Email">Username</label>
+              <input type="email" name="userName" class="form-control" id="Email" aria-describedby="emailHelp"
+                placeholder="Enter Username">
+            </div>
 
-  <div class="form-group">
-    <a href="forgotPassword.php" class="text-danger font-weight-bold" name="forget_password">Forgot your password?</a>
-  </div>
+            <div class="form-group">
+              <label for="Password">Password</label>
+              <input type="password" name="password" class="form-control" id="Password" placeholder="Enter Password">
+            </div>
 
-  <button type="submit" class="btn btn-primary btn-block" name="login">Login</button>
-  <h6 class="mt-3">Not have an Account? <a href="register.php"> Create Account Here</a></h6>
+            <div class="form-group">
+              <a href="forgotPassword.php" class="text-danger font-weight-bold" name="forget_password">Forgot your
+                password?</a>
+            </div>
 
-</form>
+            <button type="submit" class="btn btn-primary btn-block" name="login">Login</button>
+            <h6 class="mt-3">Not have an Account? <a href="register.php"> Create Account Here</a></h6>
 
-</div>
-</section>
-</div>
-</main>
+          </form>
+
+        </div>
+      </section>
+    </div>
+  </main>
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+
 </html>
