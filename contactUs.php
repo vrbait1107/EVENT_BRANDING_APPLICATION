70e8caf3da81ed5fb564e889ea5118b9889c10bb
Vishal_Bait <vishalbait01@gmail.com>
2020-04-11 22:46:05 +0530

    Add Google recaptcha in contact us
    added google recaptcha in contact us also added avoid sql and cross site scripting function also added mail sending function for contacting the authorized person for users query and questions

--- contactUs.php
+++ contactUs.php
@@ -23,6 +23,9 @@ if(!isset($_SESSION['user'])) {
     <link rel="stylesheet" href="css/contactUs.css">
     <!-- Font-Awesome css   -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Google Recaptcha -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 
 
 </head>
@@ -33,119 +36,166 @@ if(!isset($_SESSION['user'])) {
     <?php include_once "navbar.php"; ?>
 
 
-    <!--Section: Contact v.2-->
-    <section class="mb-4">
+    <?php
 
-        <!--Section heading-->
-        <h2 class="h1-responsive font-weight-bold text-center my-4 text-uppercase text-primary animated zoomIn slow">
+if(isset($_POST['submit'])){
+
+            $name = $_POST['name'];
+            $contactEmail = $_POST['email'];
+            $contactSubject = $_POST['subject'];
+            $contactMessage = $_POST['message'];
+
+             $name = mysqli_real_escape_string($conn,$name);
+            $contactEmail =  mysqli_real_escape_string($conn,$contactEmail);
+            $contactSubject = mysqli_real_escape_string($conn,$contactSubject);
+            $contactMessage =  mysqli_real_escape_string($conn,$contactMessage);
+
+            $name = htmlentities($name);
+            $contactEmail =  htmlentities($contactEmail);
+            $contactSubject = htmlentities($contactSubject);
+            $contactMessage =  htmlentities($contactMessage);
+
+
+             if(isset($_POST['g-recaptcha-response'])) {
+
+            $secretKey = "6LdGougUAAAAAHPUmWu-g9UgB9QbHpHnjyh5PxXg";
+            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretKey.'&response='.$_POST['g-recaptcha-response']);
+            $response = json_decode($verifyResponse);
+
+                 if($response->success){
+
+            date_default_timezone_set('Etc/UTC');
+            require 'PHPMailer/PHPMailerAutoload.php';
+
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+            $mail->SMTPDebug = 0;
+            $mail->Debugoutput = 'html';
+            $mail->Host = 'smtp.gmail.com';
+            $mail->Port = 587;
+            $mail->SMTPSecure = 'tls';
+            $mail->SMTPAuth = true;
+            $mail->Username = "vishalbait02@gmail.com";
+            $mail->Password = "9921172153";
+            $mail->setFrom($contactEmail, $name);
+            $mail->addReplyTo('non-reply@gmail.com', 'GIT SHODH 2K20');
+            $mail->addAddress("vishalbait01@gmail.com", "Vishal Bait");
+            $mail->Subject = $contactSubject;
+
+            //Read an HTML message body from an external file, convert referenced images to embedded,
+            //convert HTML into a basic plain-text alternative body
+            $mail->msgHTML("<!doctype html><html><body>$contactMessage</body></html>");
+            $mail->AltBody = $contactMessage;
+
+            if (!$mail->send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+            } else {
+            echo "
+            <script>Swal.fire({
+                    icon: 'Success',
+                    title: 'Success',
+                    text: 'Email Sent'
+                })</script>";         }
+
+             function save_mail($mail) {
+            //You can change 'Sent Mail' to any other folder or tag
+            $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
+
+            //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
+            $imapStream = imap_open($path, $mail->Username, $mail->Password);
+
+            $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
+            imap_close($imapStream);
+
+            return $result;
+            }
+
+                }// if $response
+
+        else{
+        echo "<script>Swal.fire({
+                icon: 'warning',
+                title: 'Google Recaptcha Error',
+                text: 'Please fill Google Recaptcha'
+            })</script>";
+        }
+
+    }// if(isset($_POST['g-recaptcha-response']))
+
+}
+
+
+?>
+
+
+    <main class="mb-5 container">
+
+        <h2 class="font-weight-bold text-center my-4 text-uppercase text-primary animated zoomIn slow">
             Contact us</h2>
-        <!--Section description-->
-        <h5 class="text-center w-responsive mx-auto mb-5 animated zoomIn slow">Do you have any questions? Please do not
+
+        <h5 class="text-center w-responsive mx-auto mb-3 animated zoomIn slow">Do you have any questions? Please do not
             hesitate to contact us directly. Our team will come back to you within
             a matter of hours to help you.</h5>
 
         <div class="row">
 
-            <!--Grid column-->
-            <div class="col-md-9 mb-md-0 mb-5 wow zoomIn slow">
+            <section class="col-md-8 wow zoomIn slow">
+
                 <form id="contact-form" name="contact-form" action="" method="POST"
                     onsubmit="return validateFormContact()">
 
-                    <!--Grid row-->
-                    <div class="row">
-
-                        <!--Grid column-->
-                        <div class="col-md-6">
-                            <div class="md-form mb-0">
-                                <label for="name" class="">Your name</label>
-                                <input type="text" id="name" name="name" class="form-control">
-
-                            </div>
-                        </div>
-                        <!--Grid column-->
-
-                        <!--Grid column-->
-                        <div class="col-md-6">
-                            <div class="md-form mb-0">
-                                <label for="email" class="">Your email</label>
-                                <input type="text" id="email" name="email" class="form-control">
-
-                            </div>
-                            <br>
-                        </div>
-                        <!--Grid column-->
-
+                    <div class="form-group">
+                        <label for="name">Your Name</label>
+                        <input type="text" id="name" name="name" class="form-control">
                     </div>
-                    <!--Grid row-->
 
-                    <!--Grid row-->
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="md-form mb-0">
-                                <label for="subject" class="">Subject</label>
-                                <input type="text" id="subject" name="subject" class="form-control">
+                    <div class="form-group">
+                        <label for="email" class="">Your Email</label>
+                        <input type="text" id="email" name="email" class="form-control">
+                    </div>
 
-                            </div>
+                    <div class="form-group">
+                        <label for="subject" class="">Subject</label>
+                        <input type="text" id="subject" name="subject" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="message">Your message</label>
+                        <textarea type="text" id="message" name="message" rows="3"
+                            class="form-control md-textarea"></textarea>
+                    </div>
+
+                    <div class="text-center my-4">
+                        <div class="g-recaptcha text-center" data-sitekey="6LdGougUAAAAAG96eGund5fScrR1fouBZvyLf1RL">
                         </div>
                     </div>
-                    <br>
-                    <!--Grid row-->
 
-                    <!--Grid row-->
-                    <div class="row">
-
-                        <!--Grid column-->
-                        <div class="col-md-12">
-
-                            <div class="md-form">
-                                <label for="message">Your message</label>
-                                <textarea type="text" id="message" name="message" rows="3"
-                                    class="form-control md-textarea"></textarea>
-
-                            </div>
-                            <br>
-                            <div class="text-center text-md-left">
-                                <input class="btn btn-primary button" type="submit" name="submit" value="Send">
-                            </div>
-
-
-                        </div>
+                    <div class="text-center text-md-left">
+                        <input class="btn btn-primary rounded-pill btn-block" type="submit" name="submit" value="Send">
                     </div>
-                    <!--Grid row-->
 
                 </form>
-                <br>
+            </section>
 
-            </div>
-            <!--Grid column-->
 
-            <!--Grid column-->
-            <div class="col-md-3 text-center wow zoomIn slow">
-
-                <ul class="list-unstyled mb-0">
-                    <li><i class="fa fa-map-marker fa-3x"></i>
-                        <h5 class="text-success ">Gharda Institute of Technology Lavel-Khed, Maharashtra, INDIA</h5>
-                    </li>
-                    <br>
-
-                    <li><i class="fa fa-phone fa-3x"></i>
-                        <h5 class="text-success">+ 01 234 567 89</h5>
+            <section class="col-md-3 text-center wow zoomIn slow">
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-map-marker mb-2 fa-3x"></i>
+                        <h5 class="text-success mb-5">Gharda Institute of Technology Lavel-Khed, Maharashtra, INDIA</h5>
                     </li>
 
-                    <br>
+                    <li><i class="fa fa-phone mb-2 fa-3x"></i>
+                        <h5 class="text-success mb-5">+ 01 234 567 89</h5>
+                    </li>
 
-                    <li><i class="fa fa-envelope fa-3x"></i>
+                    <li><i class="fa fa-envelope mb-2 fa-3x"></i>
                         <h5 class="text-success">git-india.edu.gmail.com</h5>
                     </li>
                 </ul>
-            </div>
-            <!--Grid column-->
+            </section>
 
         </div>
-
-    </section>
-
-    <!--Section: Contact v.2-->
+    </main>
 
     <!--Footer.PHP-->
     <?php include_once 'footer.php'; ?>
@@ -159,18 +209,20 @@ if(!isset($_SESSION['user'])) {
     <!--Form Validation-->
     <script src="js/form-validation.js"> </script>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
+
+    <!--Popper.js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous"></script>
+
+    <!--Bootstrap JS-->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-
 
 </body>
 
