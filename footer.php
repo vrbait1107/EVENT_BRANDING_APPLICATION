9b91668d4346cfba2bd5894906f06f38341cdb7c
Vishal_Bait <vishalbait01@gmail.com>
2020-04-15 09:54:10 +0530

    Add Verify Certificate field in footer
    footer must contain all pages related to website so verify certificate page added

--- footer.php
+++ footer.php
@@ -1,65 +1,63 @@
 <footer class="p-5 text-white bg-dark">
-        <div class="container">
-            <div class="row">
+    <div class="container">
+        <div class="row">
 
-                <div class="col-md-4">
-                    <h5 class="mb-3 text-uppercase">Pages</h5>
-                    <ul class="footerList">
-                        <li><a  class="text-white" href="welcome.php">Home</a></li>
-                        <li><a  class="text-white" href="eventPage.php">Events</a></li>
-                        <li><a  class="text-white" href="redirect.php">Get Certificate</a></li>
-                        <li><a  class="text-white" href="developers.php">Developers</a></li>
+            <div class="col-md-4">
+                <h5 class="mb-3 text-uppercase">Pages</h5>
+                <ul class="footerList">
+                    <li><a class="text-white" href="welcome.php">Home</a></li>
+                    <li><a class="text-white" href="eventPage.php">Events</a></li>
+                    <li><a class="text-white" href="redirect.php">Get Certificate</a></li>
+                    <li><a class="text-white" href="certificateVerfication.php">Verify Certificate</a></li>
+                    <li><a class="text-white" href="developers.php">Developers</a></li>
 
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h5 class="mb-3 text-uppercase">About</h5>
-                    <ul class="footerList">
-                        <li><a  class="text-white" href="aboutPage.php">About</a></li>
-                        <li><a class="text-white" href="contactUs.php">Contact Us</a></li>
-                        <li><a class="text-white" href="#">Terms & Condition </a></li>
-                        <li><a class="text-white" href="#">Privacy policy</a></li>
+                </ul>
+            </div>
 
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h5 class="mb-3 text-uppercase">Join Us</h5>
-                    <a href="#" target="_blank">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-facebook-f fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </a>
+            <div class="col-md-4">
+                <h5 class="mb-3 text-uppercase">About</h5>
+                <ul class="footerList">
+                    <li><a class="text-white" href="aboutPage.php">About</a></li>
+                    <li><a class="text-white" href="contactUs.php">Contact Us</a></li>
+                    <li><a class="text-white" href="#">Terms & Condition </a></li>
+                    <li><a class="text-white" href="#">Privacy policy</a></li>
 
-                    <a href="#" target="_blank">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </a>
-                    <a href="#" target="_blank">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </a>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5 class="mb-3 text-uppercase">Join Us</h5>
+                <a href="#" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-facebook-f fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
 
-                    <a href="#" target="_blank">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </a>
+                <a href="#" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+                <a href="#" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
 
-                    <a href="#" target="_blank">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </a>
-                </div>
+                <a href="#" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+
+                <a href="#" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
             </div>
         </div>
-        <div class="text-center">
-            &copy; 2020 GIT SHODH All Rights Reserved.
-        </div>
-    </footer>
-
-
-
-    
+    </div>
+    <div class="text-center">
+        &copy; 2020 GIT SHODH All Rights Reserved.
+    </div>
+</footer>
