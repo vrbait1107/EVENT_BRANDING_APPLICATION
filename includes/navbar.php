ec6cd9330214bf993c263ab9163c1db1813d626c
Vishal_Bait <vishalbait01@gmail.com>
2020-07-08 11:11:02 +0530

    Move logout functionality to ajax
    *Logout with Ajax
    1. Sometimes User by mistakely click on logout button and he logouts, to prevent such activity we add warniing button so user can confirm for logout.
    2. Add userLogout.js
    3. Modified logout.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -69,7 +69,7 @@
                     <a class="dropdown-item" href="userProfile.php">My Profile</a>
                     <a class="dropdown-item" href="userAccount.php">Account</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="logout.php">Log out</a>
+                    <a class="dropdown-item" id="userLogout" href="#">Log out</a>
                 </div>
             </li>
         </ul>
