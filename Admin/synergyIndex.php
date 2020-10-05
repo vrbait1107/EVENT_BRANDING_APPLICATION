b1e92ffccb17fc65c9af2a1f15662095a2c7bfc4
Vishal_Bait <vishalbait01@gmail.com>
2020-10-05 13:24:43 +0530

    Synergy Admin can view participant count 1. Synergy admin can view total participant count on his/her panel. 2. Synergy admin can also view participant count event wise

--- Admin/synergyIndex.php
+++ Admin/synergyIndex.php
@@ -14,6 +14,43 @@ if (!isset($_SESSION['Admin'])) {
 }
 ?>
 
+<?php
+try {
+
+    //------------------------->>  DISPLAY PARTICIPATION COUNT
+
+    $sql = "SELECT * FROM synergy_event_registrations";
+    $result = $conn->prepare($sql);
+    $result->execute();
+    $rowParticipantCount = $result->rowCount();
+
+    //------------------------->>  EXTRACT EVENT NAME IN ARRAY
+
+    $sql1 = "SELECT eventName FROM synergy_events_details";
+    $result1 = $conn->prepare($sql1);
+    $result1->execute();
+    $eventArray = $result1->fetch(PDO::FETCH_ASSOC);
+
+    //------------------------->>  DISPLAY PARTICIPATION COUNT EVENT WISE
+
+    function participantCount($conn, $event)
+    {
+        $sql = "SELECT * FROM synergy_event_registrations WHERE eventName = :event";
+        $result = $conn->prepare($sql);
+        $result->bindValue(":event", $event);
+        $result->execute();
+        $count = $result->rowCount();
+        return $count;
+    }
+
+} catch (PDOException $e) {
+    echo "<script>alert('We are sorry, there seems to be a problem with our systems. Please try again.');</script>";
+    # Development Purpose Error Only
+    echo "Error " . $e->getMessage();
+}
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -44,8 +81,63 @@ include_once "includes/adminNavbar.php";
     <div id="layoutSidenav_content">
 
         <main class="container">
-                <h1 class="mt-4 font-Staatliches-heading text-center text-info">Dashboard for Synergy Administrator</h1>
-                <hr />
+            <h1 class="mt-2 font-Staatliches-heading text-center text-info">Dashboard for Synergy Administrator</h1>
+            <hr />
+
+            <div class="row">
+                <!-- Total Participation of the Events -->
+                <section class="col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Total Participations</div>
+                                    <div class="h5 mb-0 font-weight-bold"><?php echo $rowParticipantCount; ?></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-users fa-3x text-warning"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <hr />
+
+            <h4 class="text-center text-info text-uppercase my-4 font-time">Participation Count Event Wise</h4>
+
+            <div class="row">
+
+                <!--Participant Count Event Wise -->
+
+                <?php foreach ($eventArray as $eventName): ?>
+
+                <section class="col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Student Participants <br />
+                                        <span class="text-danger"><?php echo $eventName; ?></span>
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        <?php echo participantCount($conn, $eventName) ?></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-users fa-3x text-warning"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <?php endforeach;?>
+
+            </div>
+
         </main>
 
         <!--Include Admin Footer-->
