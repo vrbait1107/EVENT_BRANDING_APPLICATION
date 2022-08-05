5dd71b51cc9608e11e51121bd3557e40a6fd2e53
Vishal Bait <vishalbait01@gamil.com>
2022-08-05 12:11:52 +0530

    optimize and make code more readable using early returns

--- ajaxHandlerPHP/ajaxContactUs.php
+++ ajaxHandlerPHP/ajaxContactUs.php
@@ -1,115 +1,104 @@
 <?php
 
-// --------------------------->> DB CONFIG
 require_once "../config/configPDO.php";
 
-// --------------------------->> SESSION START
 session_start();
 
-// --------------------------->> EXTRACT DATA
 extract($_POST);
 
-// --------------------------->> SECRETS
 require_once "../config/Secret.php";
 
-// ------------------------------>> CHECKING USER
-
-if (!isset($_SESSION['user'])):
-    header("location:../login.php");
-endif;
+if (!isset($_SESSION['user'])) header("location:../login.php");
 
 try {
 
     if (isset($_POST['submit'])) {
 
-        if (isset($_POST['captcha'])) {
-
-            $name = $_POST["name"];
-            $email = $_POST["email"];
-            $subject = $_POST["subject"];
-            $message = $_POST["message"];
-
-            if (empty($name)) {
-                echo "<script>Swal.fire({
-						icon: 'warning',
-			            title: 'Required',
-						text: 'Name cannot be empty',
-					})</script>";
-                return;
-            }
-
-            if (empty($email)) {
-                echo "<script>Swal.fire({
-						icon: 'warning',
-						title: 'Required',
-						text: 'Email cannot be empty',
-					})</script>";
-                return;
-            }
-
-            if (empty($subject)) {
-                echo "<script>Swal.fire({
-						icon: 'warning',
-						title: 'Required',
-						text: 'Subject cannot be empty',
-					})</script>";
-                return;
-            }
-
-            if (empty($message)) {
-                echo "<script>Swal.fire({
-						icon: 'warning',
-						title: 'Required',
-						text: 'Subject cannot be empty',
-					})</script>";
-                return;
-            }
-
-            $name = htmlspecialchars($_POST["name"]);
-            $email = htmlspecialchars($_POST["email"]);
-            $subject = htmlspecialchars($_POST["subject"]);
-            $message = $_POST["message"];
-
-            $secretKey = $recaptchaSecretKey;
-
-            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey . '&response=' . $_POST['captcha']);
-
-            $response = json_decode($verifyResponse);
-
-            if ($response->success) {
-
-                /* PHP MAILER CODE */
-
-                include_once "../emailCode/emailContactUs.php";
-
-                if (!$mail->send()) {
-                    echo "Mailer Error: " . $mail->ErrorInfo;
-
-                } else {
-                    echo "<script>Swal.fire({
-                        icon: 'success',
-                        title: 'Success',
-                        text: 'Email Sent'
-                    })</script>";
-                }
-
-            } else {
-                echo "<script>Swal.fire({
-                    icon: 'warning',
-                    title: 'Google Recaptcha Error',
-                    text: 'Please fill Google Recaptcha Properly'
-                })</script>";
-            }
-
-        } else {
+        if (!isset($_POST['captcha'])) {
             echo "<script>Swal.fire({
-                    icon: 'warning',
-                    title: 'Google Recaptcha Error',
-                    text: 'Please fill Google Recaptcha Properly'
-                })</script>";
-
+                icon: 'warning',
+                title: 'Google Recaptcha Error',
+                text: 'Please fill Google Recaptcha Properly'
+            })</script>";
+            exit;
         }
 
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
+
+        if (empty($name)) {
+            echo "<script>Swal.fire({
+					icon: 'warning',
+			        title: 'Required',
+					text: 'Name cannot be empty',
+				})</script>";
+            exit;
+        }
+
+        if (empty($email)) {
+            echo "<script>Swal.fire({
+					icon: 'warning',
+					title: 'Required',
+					text: 'Email cannot be empty',
+				})</script>";
+            exit;
+        }
+
+        if (empty($subject)) {
+            echo "<script>Swal.fire({
+					icon: 'warning',
+					title: 'Required',
+					text: 'Subject cannot be empty',
+				})</script>";
+            exit;
+        }
+
+        if (empty($message)) {
+            echo "<script>Swal.fire({
+					icon: 'warning',
+					title: 'Required',
+					text: 'Subject cannot be empty',
+				})</script>";
+            exit;
+        }
+
+        $name = htmlspecialchars($_POST["name"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $subject = htmlspecialchars($_POST["subject"]);
+        $message = $_POST["message"];
+
+        $secretKey = $recaptchaSecretKey;
+
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey . '&response=' . $_POST['captcha']);
+
+        $response = json_decode($verifyResponse);
+
+        if (!$response->success) {
+            echo "<script>Swal.fire({
+                icon: 'warning',
+                title: 'Google Recaptcha Error',
+                text: 'Please fill Google Recaptcha Properly'
+            })</script>";
+            exit;
+        }
+
+        /* PHP MAILER CODE */
+
+        include_once "../emailCode/emailContactUs.php";
+
+        if (!$mail->send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+            exit;
+        }
+
+        echo "<script>Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'Email Sent'
+            })</script>";
+        exit;
     }
 
 } catch (PDOException $e) {
@@ -118,5 +107,4 @@ try {
     echo "Error " . $e->getMessage();
 }
 
-// CLOSE DATABASE CONNECTION
 $conn = null;
